e03af5b2d5202e416f390fb995e46eafae7519c9
Tessa Woodard <tessawoodard@outlook.com>
2020-08-12 11:48:09 -0400

    donedone

---
+++ Part2/index.js
@@ -0,0 +1,13 @@
+console.log('The house always wins!')
+
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+console.log(idInput);
+console.log(colorInput);
+
+function setCard() {
+    const card = document.getElementById(idInput.value);
+    card.style.color = colorInput.value;
+    console.log(card);
+  }
